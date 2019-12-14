4a57621d54746bb4306f812235074722903516fe
Rohan Singh Sisodiya <rohanrajput32843@gmail.com>
2019-12-14 20:06:26 +0530

    commit2

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express =require('express')
-const app = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
+
+const app = express()
+
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology: true
+}).then(()=>console.log("DB Connected"));
 
 app.get('/',(req,res)=>{
     res.send("hello from node");
