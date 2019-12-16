073553510c6afc286c59fcbbbf83858202c313b8
Rohan Singh Sisodiya <rohanrajput32843@gmail.com>
2019-12-16 18:36:43 +0530

    commit6

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {create,productById,read,remove,update} = require('../controllers/product');
+const {create,productById,read,remove,update,list,listRelated} = require('../controllers/product');
 const {requireSignIn,isAdmin,isAuth} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
 router.get('/product/:productId',read)
 router.post("/product/create/:userId",requireSignIn,isAuth,isAdmin,create);
 
+router.get('/products',list)
+router.get('/products/related/:productId',listRelated)
 router.param("userId",userById);
 router.param("productId",productById);
 
