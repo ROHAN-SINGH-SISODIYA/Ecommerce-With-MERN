0c3a74e47e47cb19320159f1afda5a5c1549dc2e
Rohan Rajput <rohanrajput32843@gmail.com>
2019-12-16 23:06:02 +0530

    complete api

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {create,productById,read,remove,update,list,listRelated} = require('../controllers/product');
+const {create,
+       productById,
+       read,remove,
+       update,list,
+       listRelated,
+       listCategories,
+       listBySearch,
+       getPhoto} = require('../controllers/product');
 const {requireSignIn,isAdmin,isAuth} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
@@ -10,11 +17,15 @@ router.post("/product/create/:userId",requireSignIn,isAuth,isAdmin,create);
 
 router.get('/products',list)
 router.get('/products/related/:productId',listRelated)
+router.get('/products/categories',listCategories)
+
 router.param("userId",userById);
 router.param("productId",productById);
 
 router.delete("/product/:productId/:userId", requireSignIn,isAuth,isAdmin,remove)
 router.put("/product/:productId/:userId", requireSignIn,isAuth,isAdmin,update)
 
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId",getPhoto)
 
 module.exports=router;
